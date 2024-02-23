98f3360801e72cca38e10ccbafc37d1ae1216a56
Jason Baker <jason@capsule42.com>
2024-02-23 12:09:17 -0800

    perf(docs): refactor example scripts to new system

--- demo/api.js
+++ demo/api.js
@@ -1,16 +1,20 @@
+/* eslint-disable jsdoc/require-jsdoc, no-magic-numbers, no-param-reassign */
+
 import { centerElementExample } from '../apiExamples/centerElement';
 
-export function initCarouselApiExamples(initCount) {
+export function initExamples(initCount) {
   initCount = initCount || 0;
 
   try {
+    // javascript example function calls to be added here upon creation to test examples
     centerElementExample();
-  } catch {
+  } catch (err) {
     if (initCount <= 20) {
       // setTimeout handles issue where content is sometimes loaded after the functions get called
       setTimeout(() => {
-        initCarouselApiExamples(initCount + 1);
+        initExamples(initCount + 1);
       }, 100);
     }
   }
 }
+
