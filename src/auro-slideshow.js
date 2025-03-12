2cc19657ad13eb56ee16541662185271834c125c
Jordan Jones <jmjones2463@gmail.com>
2025-03-12 09:43:37 -0700

    perf: make Swiper config use API properties

--- src/auro-slideshow.js
+++ src/auro-slideshow.js
@@ -5,47 +5,109 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import slideshowCss from './slideshow-css.js';
 
 export class AuroSlideshow extends LitElement {
+  constructor() {
+    super();
+
+    this.autoplay = undefined;
+    this.loop = false;
+    this.slidesPerView = 1;
+    this.spaceBetweenSlides = 30;
+  }
+
   static get styles() {
     return [slideshowCss];
   }
 
+  static get properties() {
+    return {
+      /**
+       * The time in milliseconds between each slide change. Defaults to undefined.
+       */
+      autoplay: {
+        type: Number,
+        reflect: true
+      },
+
+      /**
+       * If true, the slideshow will loop back to the first slide after reaching the last slide. Defaults to false.
+       */
+      loop: {
+        type: Boolean,
+        reflect: true
+      },
+
+      /**
+       * The number of slides per view. Defaults to 1.
+       */
+      slidesPerView: {
+        type: String,
+        reflect: true
+      },
+
+      /**
+       * The pixel distance between slides when multiple slides are in view.
+       */
+      spaceBetweenSlides: {
+        type: Number
+      }
+    }
+  }
+
   firstUpdated() {
+    this.initializeSwiper();
+  }
+
+  initializeSwiper() {
     const swiperElement = this.shadowRoot.querySelector('.swiper');
     const nextButton = this.shadowRoot.querySelector('.swiper-button-next');
     const prevButton = this.shadowRoot.querySelector('.swiper-button-prev');
     const paginationEl = this.shadowRoot.querySelector('.swiper-pagination');
-  
-    if (swiperElement) {
-      const swiper = new Swiper(swiperElement, {
-        modules: [Navigation, Pagination, Autoplay],
-        loop: true,
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
+    const progressBar = this.shadowRoot.querySelector('.progress-bar');
+
+    const swiperConfig = {
+      modules: [Navigation, Pagination],
+      loop: this.loop,
+      slidesPerView: this.slidesPerView,
+      spaceBetween: this.spaceBetweenSlides,
+      pagination: {
+        el: paginationEl,
+        clickable: true,
+      },
+      navigation: {
+        nextEl: nextButton,
+        prevEl: prevButton,
+      },
+      on: {
+        slideChangeTransitionStart() {
+          progressBar.style.width = '0%';
         },
-        pagination: {
-          el: paginationEl,
-          clickable: true,
-        },
-        navigation: {
-          nextEl: nextButton,
-          prevEl: prevButton,
+        slideChangeTransitionEnd() {
+          progressBar.style.width = '100%';
         }
-      });
-  
-      // Add custom event listeners if necessary
-      nextButton.addEventListener('click', () => {
-        swiper.slideNext();
-      });
-  
-      prevButton.addEventListener('click', () => {
-        swiper.slidePrev();
-      });
-    } else {
-      console.error("Swiper container not found!");
+      }
+    };
+
+    if (this.autoplay) {
+      swiperConfig.modules.push(Autoplay);
+      swiperConfig.autoplay = {
+        delay: this.autoplay,
+        disableOnInteraction: false,
+      };
+
+      swiperConfig.on.autoplayTimeLeft = (_, _, progress) => {
+        progressBar.style.width = `${(1 - progress) * 100}%`;
+      };
     }
+
+    const swiper = new Swiper(swiperElement, swiperConfig);
+
+    nextButton.addEventListener('click', () => {
+      swiper.slideNext();
+    });
+
+    prevButton.addEventListener('click', () => {
+      swiper.slidePrev();
+    });
   }
 
   render() {
@@ -59,6 +121,10 @@ export class AuroSlideshow extends LitElement {
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
         <div class="swiper-pagination"></div>
+
+        <div class="progress-bar-container">
+          <div class="progress-bar"></div>
+        </div>
       </div>
     `;
   }
