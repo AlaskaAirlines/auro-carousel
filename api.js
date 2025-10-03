e21aae94fc76e235ab7ad67fec91de106326f616
rmenner <rmenner@users.noreply.github.com>
2025-10-03 22:52:33 +0000

    deploy: 0cd9cfb8586624cb41505cd540f889a85749fe8c

---
+++ api.js
@@ -0,0 +1,18 @@
+import { centerElementExample } from "../apiExamples/centerElement";
+import "../src/registered";
+
+export function initExamples(initCount) {
+  //biome-ignore lint: going to refactor this file later
+  initCount = initCount || 0;
+
+  try {
+    centerElementExample();
+  } catch {
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+import { AuroCarousel } from "../src/index";
+
+AuroCarousel.register();
+AuroCarousel.register("custom-carousel");
+
+export function initExamples(initCount) {
+  //biome-ignore lint: going to refactor this file later
+  initCount = initCount || 0;
+
+  try {
+  } catch {
+    //biome-ignore lint: going to refactor this file later
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
